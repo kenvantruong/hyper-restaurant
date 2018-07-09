1c973aac8d01c0044667fe44d1298c158afa78c3
Ken Truong <kenvantruong@gmail.com>
2018-07-09 00:42:53 -0700

    first commit

---
+++ assets/js/hyper/components/Header.js
@@ -0,0 +1,11 @@
+import {h, app} from 'hyperapp'
+
+export default function App({state, actions}) {
+  return (
+    <header>
+      Header comp
+    </header>
+  )
+}
+// <Header state={state} actions={actions}/>
+// <Button state={state} actions={actions}/>
---
+++ public/js/components/hyper.js
@@ -0,0 +1,146 @@
+webpackJsonp([0],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var actions = exports.actions = {
+  up: up,
+  intro: intro,
+  showMenu: showMenu
+};
+
+function up(state, actions) {
+  return { count: state.count + 1 };
+}
+
+function showMenu() {}
+
+function intro(state, actions) {
+  console.log('Just ran my first action');
+  return { count: state.count + 1 };
+}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = App;
+
+var _hyperapp = __webpack_require__(0);
+
+var _Header = __webpack_require__(4);
+
+var _Header2 = _interopRequireDefault(_Header);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function App(_ref) {
+  var state = _ref.state,
+      actions = _ref.actions;
+
+  return (0, _hyperapp.h)(
+    'div',
+    { 'class': 'app' },
+    (0, _hyperapp.h)(_Header2.default, { state: state, actions: actions })
+  );
+}
+// <Header state={state} actions={actions}/>
+// <Button state={state} actions={actions}/>
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var globalState = exports.globalState = {
+  count: 0
+};
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = App;
+
+var _hyperapp = __webpack_require__(0);
+
+function App(_ref) {
+  var state = _ref.state,
+      actions = _ref.actions;
+
+  return (0, _hyperapp.h)(
+    'header',
+    null,
+    'Header comp'
+  );
+}
+// <Header state={state} actions={actions}/>
+// <Button state={state} actions={actions}/>
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _hyperapp = __webpack_require__(0);
+
+var _actions = __webpack_require__(1);
+
+var _globalState = __webpack_require__(3);
+
+var _App = __webpack_require__(2);
+
+var _App2 = _interopRequireDefault(_App);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _hyperapp.app)({
+  state: { globalState: _globalState.globalState },
+  view: function view(state, actions) {
+    return (0, _hyperapp.h)(_App2.default, { state: state, actions: actions });
+  },
+  root: document.getElementById('app'),
+  actions: _actions.actions,
+  events: {
+    action: function action(state, actions, _ref) {
+      var name = _ref.name,
+          data = _ref.data;
+
+      console.group("Action Info");
+      console.log("Name:", name);
+      console.log("Data:", data);
+      console.groupEnd();
+    },
+    load: function load(state, actions) {
+      actions.intro();
+    }
+  }
+});
+
+/***/ })
+],[5]);
